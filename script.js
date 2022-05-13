3c6414574b8e6b0128ac374735dbc53f1a67ddfd
satoshi <highfive0228@gmail.com>
2022-05-13 21:29:26 +0300

    reworked with classses oop

--- script.js
+++ script.js
@@ -1,161 +1,134 @@
-let myLibrary = [];
+const lib = (function() {
+  let myLibrary = [];
+  let count = 0;
+  let scount = 1; 
 
-let count = 0;
-let scount = 1;   //shelf count
-let cshelf = 0;  
-
-
-setTimeout(() => {          //waiting for the DOM to load
-
-  let add = document.getElementById("add");   //"add new books" button
+  const add = document.getElementById("add");   //"add new books" button
   add.addEventListener("click", function() {
     document.getElementById("form").style.display = "flex";
   });
 
-  let cls = document.getElementById("b");   //"close" button
+  const cls = document.getElementById("b");   //"close" button
   cls.addEventListener("click", function(){
     document.getElementById("f").reset();
     document.getElementById("form").style.display = "none";
   });
 
-  let subm = document.getElementById("a");   //"add" button
+  const subm = document.getElementById("a");   //"add" button
   subm.addEventListener("click", function(){
     if(title.checkValidity() && aname.checkValidity() && number.checkValidity()){
-      newBook(title.value, aname.value, number.value, r1.checked);    //adds a book to the list
-      addbook(title.value, aname.value, number.value, r1.checked);    //adds a book to the DOM
+      let book = new Book(title.value, aname.value, number.value, r1.checked);
+      book.cacheTemplate();
+      book.render();
+      book.addBook();
+      book.setEventListeners();
+      myLibrary.push(book);
   }});
 
 
-  let list = document.getElementById('list');   
-  let table = document.getElementById('bookslist');
+  const list = document.getElementById('list');   //"list of books" button
+  const table = document.getElementById('bookslist');
   list.addEventListener("click", function() {
-  document.getElementById('table').style.display = "flex";
-  let count1 = 1;
-
-  for(let i of myLibrary) {                                           //"list of books" button
-    table.insertAdjacentHTML('beforeend', "<tbody class = 'l'></tbody>");
-    let p1 = document.createElement('td');
-    p1.innerText = String(count1);
-    table.lastElementChild.appendChild(p1);
-    for(let y in i) {
-
-      if(typeof(i[y]) == "function"){
-        break
+    document.getElementById('table').style.display = "flex";
+    for(let i of myLibrary) {                                           
+      table.insertAdjacentHTML('beforeend', "<tbody class = 'l'></tbody>");
+      for(let y in i) {
+        if(typeof(i[y]) == "object"){
+          break;
+        }
+        let p = document.createElement('td');
+        if(typeof(i[y]) == "boolean"){
+          if(i[y] === true){
+            p.innerText = "read";
+          } else p.innerText = "unread";
+        } else p.innerText = i[y];
+        table.lastElementChild.appendChild(p);
       }
-      let p = document.createElement('td');
-      if(typeof(i[y]) == "boolean"){
-        if(i[y] === true){
-          p.innerText = "read";
-        } else p.innerText = "unread";
-      } else p.innerText = i[y];
-      table.lastElementChild.appendChild(p);
     }
-    count1++;
-  }
     document.querySelector('main').style.display = "none";
-  })
+  });
 
   document.getElementById('close').addEventListener('click', function() {   //closes the list
     document.querySelector('main').style.display = "flex";
     document.querySelectorAll('.l').forEach(e => e.remove());
     document.getElementById('table').style.display = "none";
-  })
-}, 5);
-
-
-
-
-function newBook(title, author, pagenum, read) {    //ADDS A BOOK TO AN ARRAY OF OBJECTS
-  let a = {
-    title: title,
-    author: author,
-    pagenum: pagenum,
-    read: read,
-    swit(){
-      if(this.read === true){
-        this.read = false
-      } else this.read = true;
-    }
-  }
-  myLibrary.push(a);
-  count++;
-}
-
-function adddel(el) {        //adds functionality to the "delete" button
-  el.addEventListener("click", function() {
-    let y = Number(this.parentNode.parentNode.parentNode.id);
-    let i = Number(this.parentNode.parentNode.parentNode.parentNode.parentNode.id.slice(1)) + 1
-    for(let i = y+1; i < count+1; i++){       //shifts ids before node's removal
-      document.getElementById(`${i}`).id = Number(document.getElementById(`${i}`).id) - 1;
-    }
-    this.parentNode.parentNode.parentNode.remove();
-    myLibrary.splice(y-1, 1)  //also delete book from library
-    count = count - 1;
-
-    while(document.getElementById(`s${i}`) !== null) {      //makes sure books in other shelves also shift
-      let c = document.getElementById(`s${i}`).firstElementChild.firstElementChild.cloneNode(true);
-      document.getElementById(`s${i}`).firstElementChild.firstElementChild.remove();
-      document.getElementById(`s${i-1}`).firstElementChild.appendChild(c);
-
-      document.getElementById(`s${i-1}`).firstElementChild.lastElementChild.querySelector('.read').addEventListener("click", function() {  //add event listeners to 
-        if(this.innerText === "read"){                                                                                                     //the replicated elements
-          this.innerText = "unread";
-        } else this.innerText = "read";
-        myLibrary[Number(this.parentNode.parentNode.parentNode.id)-1].swit();
-      });
-      adddel(document.getElementById(`s${i-1}`).firstElementChild.lastElementChild.querySelector('.del'));
-      i = i + 1;
-    }
-    if(document.getElementById(`s${scount-1}`).firstElementChild.firstElementChild == null) {    //deletes an empty shelf
-      document.getElementById(`s${scount-1}`).remove(); 
-      scount = scount - 1;
-    }
   });
+  return {count, scount, myLibrary};
+})();
+
+
+let Book = class {
+    constructor(title, author, pagenum, read) {
+        this.id = lib.count;
+        Object.assign(this, {title, author, pagenum, read});
+    }
+
+    cacheTemplate() {
+      this.cpy =  document.getElementById('o').cloneNode(true);
+      this.readStatus = this.cpy.querySelector('.read');
+      this.tit = this.cpy.querySelector('.tit');  
+      this.auth = this.cpy.querySelector('.author');
+      this.pages = this.cpy.querySelector('.pages');
+      this.del = this.cpy.querySelector('.del');
+    }
+
+    render() {
+      this.cpy.removeAttribute('id');  
+      if(this.read) {
+        this.readStatus.innerText = "read";
+      } else this.readStatus.innerText = "unread";
+      this.tit.innerText = '"' + this.title + '"';
+      this.auth.innerText = "By: " + this.author;
+      this.pages.innerText = this.pagenum + " pages";
+    }
+
+    addBook() {
+      lib.count++;
+      checkShelfOverload();
+      let shelves = document.getElementsByClassName('shelf');
+      for(let i = 0; i < shelves.length; i++) {
+        if(index(shelves[i]) == lib.scount){
+          shelves[i].firstElementChild.appendChild(this.cpy);
+          return;
+        }
+      }
+    }
+
+    setEventListeners() {
+        this.del.addEventListener("click", (e) => {  //delete
+          adjustShelf(index(e.target.parentElement.parentElement.parentElement.parentElement.parentElement));
+          e.target.parentNode.parentNode.parentNode.remove();
+          lib.count = lib.count - 1;
+          for(let i = lib.myLibrary.length - 1; i >= 0; i = i - 1) {
+            if(lib.myLibrary[i].id > this.id) {
+              lib.myLibrary[i].id = lib.myLibrary[i].id - 1;
+              continue;
+            }
+            if(lib.myLibrary[i].id === this.id){
+              lib.myLibrary.splice(i, 1);
+            }
+          }
+        });
+
+        this.readStatus.addEventListener("click", () => {  //read
+          this.switchReadStatus();
+        });
+    }
+
+    switchReadStatus() {
+        if(this.read === true){
+            this.read = false
+        } else this.read = true;
+        this.render();
+    }
 }
 
-function addbook(title, author, pagenum, r) {         //adds a book to the DOM
-  if(count !== 0){ 
-    cshelf = Math.floor((count - 1) / 12);     //last shelf that has books in it
-  }
-  if((count - scount * 12 ) > 0) {       //adds a new shelf if there is no more space left
-    let main = document.querySelector("main");
-    main.insertAdjacentHTML('beforeend', `<div class = 'shelf' id = 's${cshelf}'><div class = "grid"></div></div>`);
-    scount++;
-  }
-  let shelf = document.getElementById(`s${cshelf}`);
-  let o = document.getElementById('o');    
-  let cpy = o.cloneNode(true);   //copies and pastes the tepmlate book
-  cpy.id = count;
-
-  let read = cpy.querySelector('.read')
-  let del = cpy.querySelector('.del');  ///////////////
-
-  adddel(del);
-  
-
-  read.addEventListener("click", function() {    //adds "read"/"unread" switch functionality
-    if(this.innerText === "read"){
-      this.innerText = "unread";
-    } else this.innerText = "read";
-    myLibrary[Number(this.parentNode.parentNode.parentNode.id)-1].swit();
-  });
-
-  let tit = cpy.querySelector('.tit');    //writes stuff on top of the book
-  let auth = cpy.querySelector('.author');
-  let pages = cpy.querySelector('.pages');
-  let rea = cpy.querySelector('.read')
-  if(r) {
-    rea.innerText = "read"
-  } else rea.innerText = "unread";
-  tit.innerText = '"' + title + '"';
-  auth.innerText = "By: " + author;
-  pages.innerText = pagenum + " pages";
 
 
-  shelf.firstElementChild.appendChild(cpy);  
-}
 
-function sortTable(n) {
+
+
+function sortTable(n, number) {
   let shouldSwitch;
   let switchcount = 0;
   let switching = true;
@@ -175,16 +148,32 @@ function sortTable(n) {
       let y = rows[i + 1].getElementsByTagName("td")[n];
 
       if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        if (number === false) {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
 
-          shouldSwitch= true;
-          break;
+            shouldSwitch= true;
+            break;
+          }
+        } else {
+          if (Number(x.innerHTML) > Number(y.innerHTML)) {
+
+            shouldSwitch= true;
+            break;
+          }
         }
       } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        if(number === false) {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
 
-          shouldSwitch = true;
-          break;
+            shouldSwitch = true;
+            break;
+          }
+        } else {
+          if (Number(x.innerHTML) < Number(y.innerHTML)) {
+
+            shouldSwitch = true;
+            break;
+          }
         }
       }
     }
@@ -203,3 +192,41 @@ function sortTable(n) {
     }
   }
 }
+
+
+function checkShelfOverload() {
+  if((lib.count - lib.scount * 12 ) > 0) {       //adds a new shelf if there is no more space left
+    let main = document.querySelector("main");
+    lib.scount++;
+    main.insertAdjacentHTML('beforeend', "<div class = 'shelf'><div class = 'grid'></div></div>");
+  }
+}
+
+function adjustShelf(e) {
+  for(let i = e * 12; i < lib.myLibrary.length; i++) {  //shifts the shelves
+    if(((lib.myLibrary[i].id % 12) === 0)) {
+      lib.myLibrary[i].cpy.remove();
+      let shelves = document.getElementsByClassName('shelf');
+      for(let k = 0; k < shelves.length; k++) {
+        if(index(shelves[k]) - 1 == Math.floor((lib.myLibrary[i].id - 1) / 12)){
+          shelves[k].firstElementChild.appendChild(lib.myLibrary[i].cpy);
+          break;
+        }
+      }
+    }
+  }
+  if(document.querySelector('main').lastElementChild.firstElementChild.firstElementChild == null) {    //deletes an empty shelf
+    document.querySelector('main').lastElementChild.remove(); 
+    lib.scount = lib.scount - 1;
+  }
+}
+
+function index(el) {
+  if (!el) return -1;
+  var i = 1;
+  while (el = el.previousElementSibling) {
+    i++;
+  }
+  return i;
+}
+
